9572804748bbf5a14c1a88061cd732551d66b36d
Shi Han NG <shihanng@gmail.com>
2025-05-04 13:06:49 +0900

    Show filterable file path in snacks.nvim picker
    Prior to this change, the format of snacks.nvim picker entry was
    the character of the marker.
    
    This change uses the sign, sign_highlight, and file path
    to build the entry and allow user to filter/select via the file path.

--- lua/recall/snacks.lua
+++ lua/recall/snacks.lua
@@ -4,6 +4,19 @@ local marking = require("recall.marking")
 
 local M = {}
 
+local function format(item, picker)
+  local a = require("snacks").picker.util.align
+
+  local ret = {}
+
+  ret[#ret + 1] = { a(config.opts.sign, 2), config.opts.sign_highlight }
+  ret[#ret + 1] = { " ", virtual = true }
+
+  vim.list_extend(ret, require("snacks").picker.format.file(item, picker))
+
+  return ret
+end
+
 local function finder()
   local marks = utils.sorted_global_marks()
 
@@ -11,7 +24,7 @@ local function finder()
   for _, mark in ipairs(marks) do
     table.insert(items, {
       char = mark.char,
-      text = mark.char,
+      text = mark.char .. " " .. mark.info.file,
       pos = { [1] = mark.info.pos[2], [2] = mark.info.pos[3] },
       file = mark.info.file,
     })
@@ -27,13 +40,14 @@ local function unmark_selected_entry(self, item)
     refresh = true,
   })
 end
+
 M.pick = function()
   local unmark_normal = config.opts.snacks.mappings.unmark_selected_entry.normal
   local unmark_insert = config.opts.snacks.mappings.unmark_selected_entry.insert
 
   require("snacks").picker.pick({
     title = "Global Marks",
-    format = "text",
+    format = format,
     finder = finder,
     actions = {
       unmark_selected_entry = unmark_selected_entry,
